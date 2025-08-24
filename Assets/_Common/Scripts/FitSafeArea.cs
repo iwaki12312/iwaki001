e805430ccd6ea0457a0b691135df97feb7f50d24
iwaki <iwaki_63@hotmail.co.jp>
2025-08-24 13:55:00 +0900

    ボタン配置

--- Assets/_Common/Scripts/FitSafeArea.cs
+++ Assets/_Common/Scripts/FitSafeArea.cs
@@ -1,21 +1,53 @@
-// ① SafeAreaRoot 空オブジェクトを作成
-// ② このスクリプトを付ける
 using UnityEngine;
 
+[ExecuteAlways]                               // ★ 追加：Edit時も動く
 [RequireComponent(typeof(RectTransform))]
 public class FitSafeArea : MonoBehaviour
 {
-    void Awake()
+    public enum Mode { Full, TopOnly, SymmetricH }
+    [SerializeField] Mode mode = Mode.Full;
+
+    Rect _lastArea; Vector2Int _lastRes;
+    RectTransform _rt;
+
+    void OnEnable()
     {
-        var rt = (RectTransform)transform;
-        var area = Screen.safeArea;
+        _rt = (RectTransform)transform;
+        Apply();
+    }
 
-        Vector2 min = area.position;
-        Vector2 max = area.position + area.size;
-        min.x /= Screen.width;  min.y /= Screen.height;
-        max.x /= Screen.width;  max.y /= Screen.height;
+    void LateUpdate()                          // ★ 再生中/編集中どちらでも監視
+    {
+        if (_lastArea != Screen.safeArea ||
+            _lastRes.x != Screen.width || _lastRes.y != Screen.height)
+            Apply();
+    }
 
-        rt.anchorMin = min;
-        rt.anchorMax = max;
+#if UNITY_EDITOR
+    void OnValidate() { Apply(); }             // ★ インスペクタ変更でも即反映
+#endif
+
+    void Apply()
+    {
+        var sa = Screen.safeArea;
+        _lastArea = sa; _lastRes = new Vector2Int(Screen.width, Screen.height);
+
+        float left   = sa.xMin;
+        float right  = Screen.width  - sa.xMax;
+        float bottom = sa.yMin;
+        float top    = Screen.height - sa.yMax;
+
+        if (mode == Mode.TopOnly)
+            { left = right = bottom = 0f; }
+        else if (mode == Mode.SymmetricH)
+            { float h = Mathf.Max(left, right); left = right = h; }
+
+        var amin = new Vector2(left  / Screen.width,  bottom / Screen.height);
+        var amax = new Vector2(1f - right / Screen.width, 1f - top / Screen.height);
+
+        _rt.anchorMin = amin;
+        _rt.anchorMax = amax;
+        _rt.offsetMin = Vector2.zero;
+        _rt.offsetMax = Vector2.zero;
     }
 }
