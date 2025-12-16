a35588affdcb2f584b8b15f948068953e063553c
iwaki <iwaki_63@hotmail.co.jp>
2025-12-16 14:32:14 +0900

    a

--- Assets/Scripts/UnityPurchasing/generated/AppleTangle.cs
+++ Assets/Scripts/UnityPurchasing/generated/AppleTangle.cs
@@ -1,15 +1,18 @@
 using System;
 using UnityEngine;
 
-/// <summary>
-/// AppleTangle - iOS用のダミークラス（iOS対応時に正式に生成する）
-/// </summary>
-public class AppleTangle
+namespace UnityEngine.Purchasing.Security
 {
-    private static byte[] data = System.Convert.FromBase64String("AAAA");
-    
-    public static byte[] Data()
+    /// <summary>
+    /// AppleTangle - iOS用のダミークラス（iOS対応時に正式に生成する）
+    /// </summary>
+    public class AppleTangle
     {
-        return data;
+        private static byte[] data = System.Convert.FromBase64String("AAAA");
+        
+        public static byte[] Data()
+        {
+            return data;
+        }
     }
 }
